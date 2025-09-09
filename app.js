73d18c93b2021c3fdacfbdcffdffa0c44ece49cb
NateStaxx <natejh16@gmail.com>
2025-09-09 15:26:02 -0700

    added better readability, functionality, and classes to HTML elements for screen readers to adjust to the changing information when buttons are clicked

---
+++ app.js
@@ -0,0 +1,28 @@
+//Generate Dog image API
+
+
+//Generate Cat image API
+
+
+
+//Generate Weather API
+
+
+
+//Generate Currency API
+
+
+
+//Generate Movies API
+
+
+
+//Generate GitHub API
+
+
+
+//Generate Joke API
+
+
+
+//Generate Public API
